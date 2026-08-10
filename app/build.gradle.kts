7351db50777c8f7cd479928126856070d7cdb22a
薯条 <gv92207@gmail.com>
2026-08-10 12:15:41 +0800

    chore: 版本号升至 3.1 准备发布

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ configure<ApplicationExtension> {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "3.1"
     }
     
     buildFeatures {
